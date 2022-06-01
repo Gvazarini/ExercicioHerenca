4f53300717235cd36ff0894f5f7f2655b060c436
Gabriel Vazarini <camaravazarini@gmail.com>
2022-06-01 13:02:21 -0300

    Heranca/Poliformismo

---
+++ Etiqueta.Produto/Entities/ImportProduct.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace EtiquetaProduto.Entities
+{
+    internal class ImportProduct : Product
+    {
+        public double CustomsFee { get; set; }
+        public ImportProduct()
+        {
+
+        }
+        public ImportProduct(string name, double price, double customsFee) : base(name, price)
+        {
+            CustomsFee = customsFee;
+        }
+        public double TotalPrice()
+        {
+            return Price + CustomsFee;
+        }
+        public override string PriceTag()
+        {
+            return Name + " $" + Price.ToString("F2", CultureInfo.InvariantCulture) + " Customs Fee: ($" + CustomsFee + ")" + " Total: $" + TotalPrice().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Etiqueta.Produto/Entities/UsedProduct.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace EtiquetaProduto.Entities
+{
+    internal class UsedProduct : Product
+    {
+        public DateTime ManufacturedDate { get; set; }
+        public UsedProduct() 
+        { 
+        }
+        public UsedProduct(string name, double price, DateTime manufacturedDate) : base(name, price)
+        {
+            ManufacturedDate = manufacturedDate;
+        }
+        public override string PriceTag()
+        {
+            return Name + "(used), " + Price.ToString("F2", CultureInfo.InvariantCulture)  + (" Manufactured Date: (" + ManufacturedDate.ToString("dd/MM/yyy")+ ")");
+        }
+    }
+}
